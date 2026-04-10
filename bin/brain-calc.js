0823cf2ab1abdee4bee7febc2438278e19330a73
julijaKryzh <Kryzhanovskaia.IuV@mosinzhproekt.ru>
2026-04-10 23:17:17 +0000

    Brain-calc

---
+++ bin/brain-calc.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+import { runGame } from '../src/index.js'
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
+
+const calculate = (a, b, sign) => {
+  switch (sign) {
+    case '+': return a + b
+    case '-': return a - b
+    case '*': return a * b
+    default: return null
+  }
+}
+
+const playCalc = () => {
+  const description = 'What is the result of the expression?'
+
+  const getRoundData = () => {
+    const num1 = getRandomInt(1, 20)
+    const num2 = getRandomInt(1, 10)
+    const signs = ['+', '-', '*']
+    const sign = signs[getRandomInt(0, signs.length - 1)]
+
+    const question = `${num1} ${sign} ${num2}`
+    const answer = calculate(num1, num2, sign)
+
+    return [question, answer]
+  }
+
+  runGame(description, getRoundData)
+}
+
+playCalc()
---
+++ src/index.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync'
+
+export const runGame = (description, getRoundData) => {
+  console.log('Welcome to the Brain Games!')
+  const name = readlineSync.question('May I have your name? ')
+  console.log(`Hello, ${name}!`)
+  console.log('What is the result of the expression?')
+
+  let successCount = 0
+  const roundsToWin = 3
+
+  for (let i = 0; i < roundsToWin; i++) {
+    const [question, correctAnswer] = getRoundData()
+    console.log(`Question: ${question}`)
+    const userAnswer = readlineSync.question('Your answer: ')
+
+    if (userAnswer === String(correctAnswer)) {
+      console.log('Correct!')
+      successCount++
+    }
+    else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
+      console.log(`Let's try again, ${name}!`)
+      break // Прерываем игру при ошибке
+    }
+  }
+
+  if (successCount === roundsToWin) {
+    console.log(`Congratulations, ${name}!`)
+  }
+}
