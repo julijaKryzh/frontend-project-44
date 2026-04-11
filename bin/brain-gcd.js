59bbe094af4c373dfe603e0959e5efc484751c4c
julijaKryzh <Kryzhanovskaia.IuV@mosinzhproekt.ru>
2026-04-11 23:33:37 +0000

    Brain-prime

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,23 +1,24 @@
-import { runGame } from '../src/index.js'; // Импорт общего движка
+#!/usr/bin/env node
+import { runGame } from '../src/index.js' // Импорт общего движка
 
-const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
 
-const getGcd = (a, b) => (b === 0 ? a : getGcd(b, a % b));
+const getGcd = (a, b) => (b === 0 ? a : getGcd(b, a % b))
 
 const playGcd = () => {
-  const description = 'Find the greatest common divisor of given numbers.';
+  const description = 'Find the greatest common divisor of given numbers.'
 
   const getRoundData = () => {
-    const num1 = getRandomInt(1, 100);
-    const num2 = getRandomInt(1, 100);
+    const num1 = getRandomInt(1, 100)
+    const num2 = getRandomInt(1, 100)
 
-    const question = `${num1} ${num2}`;
-    const answer = getGcd(num1, num2);
+    const question = `${num1} ${num2}`
+    const answer = getGcd(num1, num2)
 
-    return [question, String(answer)];
-  };
+    return [question, String(answer)]
+  }
 
-  runGame(description, getRoundData);
-};
+  runGame(description, getRoundData)
+}
 
-playGcd();
+playGcd()
